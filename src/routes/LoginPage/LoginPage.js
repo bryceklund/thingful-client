3a35faa1dd02e1a6155e093353311b1b4950e331
Bryce Eklund <bryce.eklund@gmail.com>
2019-08-02 11:38:14 -0700

    add basic auth

--- src/routes/LoginPage/LoginPage.js
+++ src/routes/LoginPage/LoginPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import LoginForm from '../../components/LoginForm/LoginForm'
 import { Section } from '../../components/Utils/Utils'
+import TokenService from '../../services/token-service'
 
 export default class LoginPage extends Component {
   static defaultProps = {
@@ -8,13 +9,24 @@ export default class LoginPage extends Component {
     history: {
       push: () => {},
     },
+    handleLoginSuccess = () => {
+      const { location, history } = this.props
+      const destination = (location.state || {}).from || '/'
+      history.push(destination)
+    }
   }
 
-  handleLoginSuccess = () => {
-    const { location, history } = this.props
-    const destination = (location.state || {}).from || '/'
-    history.push(destination)
+  handleSubmitBasicAuth = ev => {
+    ev.preventDefault()
+    const { user_name, password } = ev.target
+
+    TokenService.saveAuthToken(TokenService.makeBasicAuthToken(user_name.value, password.value))
+
+    user_name.value = ''
+    password.value = ''
+    this.state.handleLoginSuccess()
   }
+
 
   render() {
     return (
